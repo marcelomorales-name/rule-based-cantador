ac7fe593cf1df2bed343371fa78910a689dcd9f3
marcelomorales <marcelomorales.name@gmail.com>
2008-04-30 17:29:56 -0400

    Apocopes programados

--- src/main/java/name/marcelomorales/cantador/Cantador.java
+++ src/main/java/name/marcelomorales/cantador/Cantador.java
@@ -10,23 +10,52 @@ import java.text.ParsePosition;
 import java.util.Collections;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Locale;
+import java.util.SortedSet;
+import java.util.TreeSet;
 
 /**
  * Rule-based formatter.
  *
- * <p>This was inspired by the IBM ICU one. But I could not make it work properly with spanish. So I did my own.</p>
+ * <p>This was inspired by the IBM ICU one. But I could not make it work properly in spanish. So I did my own.</p>
  * <p>The new Version uses the Java standard interface in addition to the original one.</p>
  *
  * @author Marcelo Morales
  */
 public class Cantador extends NumberFormat {
 
+    private static final String[][] cardinals = {{"es_BO", "x"}};
+
+    /*
+     * TODO: http://en.wikipedia.org/wiki/Names_of_numbers_in_English
+     */
+    /*
+     * TODO: in spanish: female form (doscientas instead of doscientos)
+     * TODO: spanish: ordinal (male/female/"apocopado")
+     */
+    public static Cantador newCardinalInstance() {
+        return newCardinalInstance(Locale.getDefault());
+    }
+
+    public static Cantador newCardinalInstance(Locale locale) {
+        for (int i = 0; i < cardinals.length; i++) {
+            if (cardinals[i][0].equals(locale.toString())) {
+                return new Cantador(cardinals[i][1]);
+            }
+        }
+        throw new UnsupportedOperationException("Locale " + locale.toString() + " not supported yet.");
+    }
+
+    public static Cantador newInstance(String rulespec) {
+        return new Cantador(rulespec);
+    }
+
     /**
      * Default constructor.
      *
      * @param rulesSpec curently unused
      */
-    public Cantador(String rulesSpec) {
+    private Cantador(String rulesSpec) {
         /*
          * TODO: parsear rulesSpec segun un lenguaje a definirse.
          *
@@ -40,8 +69,10 @@ public class Cantador extends NumberFormat {
          * 30 => treinta[treinta y {0}]
          */
         rules = new LinkedList<Rule>();
+        apokoptos = new TreeSet<Apokoptos>();
         rules.add(new Rule(new BigInteger("0"), "cero"));
         rules.add(new Rule(new BigInteger("1"), "uno"));
+        apokoptos.add(new Apokoptos("uno", "un"));
         rules.add(new Rule(new BigInteger("2"), "dos"));
         rules.add(new Rule(new BigInteger("3"), "tres"));
         rules.add(new Rule(new BigInteger("4"), "cuatro"));
@@ -62,6 +93,7 @@ public class Cantador extends NumberFormat {
         rules.add(new Rule(new BigInteger("19"), "diecinueve"));
         rules.add(new Rule(new BigInteger("20"), "veinte"));
         rules.add(new Rule(new BigInteger("21"), "veintiuno"));
+        apokoptos.add(new Apokoptos("veintiuno", "veintiún"));
         rules.add(new Rule(new BigInteger("22"), "veintidos"));
         rules.add(new Rule(new BigInteger("23"), "veintitrés"));
         rules.add(new Rule(new BigInteger("24"), "veinticuatro"));
@@ -77,8 +109,7 @@ public class Cantador extends NumberFormat {
         rules.add(new Rule(new BigInteger("70"), "setenta", "setenta y {0}"));
         rules.add(new Rule(new BigInteger("80"), "ochenta", "ochenta y {0}"));
         rules.add(new Rule(new BigInteger("90"), "noventa", "noventa y {0}"));
-        rules.add(new Rule(new BigInteger("100"), "cien"));
-        rules.add(new Rule(new BigInteger("101"), "", false, "ciento {0}", new BigInteger("1")));
+        rules.add(new Rule(new BigInteger("100"), "cien", "ciento {0}"));
         rules.add(new Rule(new BigInteger("200"), "doscientos", "doscientos {0}"));
         rules.add(new Rule(new BigInteger("300"), "trescientos", "trescientos {0}"));
         rules.add(new Rule(new BigInteger("400"), "cuatrocientos", "cuatrocientos {0}"));
@@ -87,12 +118,14 @@ public class Cantador extends NumberFormat {
         rules.add(new Rule(new BigInteger("700"), "setecientos", "setecientos {0}"));
         rules.add(new Rule(new BigInteger("800"), "ochocientos", "ochocientos {0}"));
         rules.add(new Rule(new BigInteger("900"), "novecientos", "novecientos {0}"));
-        rules.add(new Rule(new BigInteger("1000"), "mil", "{1} mil{0}"));
+        rules.add(new Rule(new BigInteger("1000"), "un mil", "{1} mil{0}"));
         rules.add(new Rule(new BigInteger("1000000"), "un millón", "un millón {0}"));
         rules.add(new Rule(new BigInteger("2000000"), "millones", "{1} millones{0}"));
     }
 
     private List<Rule> rules;
+
+    private SortedSet<Apokoptos> apokoptos;
 
     /**
      * Sings the decimal.
@@ -110,37 +143,27 @@ public class Cantador extends NumberFormat {
     private String cantarParteEntera(BigDecimal bigDecimal) {
         BigInteger integerPart = bigDecimal.toBigInteger();
 
-        int i = Collections.binarySearch(rules, new Rule(integerPart, null, null));
+        int i = Collections.binarySearch(rules, new Rule(integerPart, null));
         if (i < 0) {
             i = -i - 2;
         }
         if (rules.get(i).recursegreater) {
-            int cuantosdigitos = rules.get(i).index.toString().length() - 1;
-            BigDecimal mayor = new BigDecimal(integerPart).movePointLeft(cuantosdigitos).setScale(0, RoundingMode.FLOOR);
-            BigDecimal menor = new BigDecimal(integerPart).subtract(mayor.movePointRight(cuantosdigitos));
-            /*
-             * TODO: como paramtrizar que el mayor no puede terminar en "uno"?
-             */
+            int howManyDigits = rules.get(i).index.toString().length() - 1;
+            BigDecimal mayor = new BigDecimal(integerPart).movePointLeft(howManyDigits).setScale(0, RoundingMode.FLOOR);
+            BigDecimal menor = new BigDecimal(integerPart).subtract(mayor.movePointRight(howManyDigits));
             String cantadoMayor = cantar(mayor);
-            if (cantadoMayor.endsWith("veintiuno")) {
-                cantadoMayor = cantadoMayor.substring(0, cantadoMayor.length() - 3).concat("ún");
-            } else if (cantadoMayor.endsWith("uno")) {
-                cantadoMayor = cantadoMayor.substring(0, cantadoMayor.length() - 1);
+            for (Apokoptos a : apokoptos) { // HAS to be sorted!
+                if (cantadoMayor.endsWith(a.haystack)) {
+                    cantadoMayor = cantadoMayor.substring(0, cantadoMayor.length() - a.haystack.length()).concat(a.needle);
+                }
             }
             if (menor.equals(BigDecimal.ZERO)) {
                 return MessageFormat.format(rules.get(i).literalcompleto, "", cantadoMayor);
             } else {
-                /*
-                 * TODO: este espacio no me gusta. No puede parametrizarse correctamente. Me imagino que algun idioma
-                 * podria no tener espacio entre los numeros.
-                 */
                 return MessageFormat.format(rules.get(i).literalcompleto, " " + cantar(menor), cantadoMayor);
             }
         }
-        /*
-         * TODO: este subtract ciertamente le quita elegancia al problema.
-         */
-        integerPart = integerPart.subtract(rules.get(i).index.subtract(rules.get(i).minus));
+        integerPart = integerPart.subtract(rules.get(i).index);
         if (integerPart.signum() > 0) {
             String parteSiguiente = cantar(new BigDecimal(integerPart));
             return MessageFormat.format(rules.get(i).literalcompleto, parteSiguiente);
@@ -165,7 +188,6 @@ public class Cantador extends NumberFormat {
             this.literal = literal;
             this.literalcompleto = "";
             this.recursegreater = false;
-            this.minus = BigInteger.ZERO;
         }
 
         public Rule(BigInteger index, String literal, String completo) {
@@ -177,20 +199,9 @@ public class Cantador extends NumberFormat {
             } else {
                 this.recursegreater = false;
             }
-            this.minus = BigInteger.ZERO;
-        }
-
-        public Rule(BigInteger index, String literal, boolean recursegreater, String extra, BigInteger mm) {
-            this.index = index;
-            this.literal = literal;
-            this.literalcompleto = literal + extra;
-            this.recursegreater = recursegreater;
-            this.minus = mm;
         }
 
         BigInteger index;
-
-        BigInteger minus;
 
         String literal;
 
@@ -200,6 +211,25 @@ public class Cantador extends NumberFormat {
 
         public int compareTo(Cantador.Rule o) {
             return index.compareTo(o.index);
+        }
+    }
+
+    private class Apokoptos implements Comparable<Apokoptos> {
+
+        public Apokoptos(String haystack, String needle) {
+            this.haystack = haystack;
+            this.needle = needle;
+            this.len = needle.length();
+        }
+
+        Integer len;
+
+        String haystack;
+
+        String needle;
+
+        public int compareTo(Apokoptos o) {
+            return o.len.compareTo(len);
         }
     }
 
