ef2f368042c3df9f4e66a0df5178241269ae77a9
marcelo <marcelo@loghost>
2008-08-25 10:10:07 -0400

    Adicionando un ordinal femenino: 0.003

--- src/main/java/name/marcelomorales/cantador/Cantador.java
+++ src/main/java/name/marcelomorales/cantador/Cantador.java
@@ -34,7 +34,7 @@ import name.marcelomorales.cantador.parser.ParseException;
  */
 public class Cantador extends CantadorSpec {
 
-    static final long serialVersionUID = -876876876876L;
+    static final long serialVersionUID = -876876876877L;
 
     private static final String[][] cardinals = {{"es_BO",
             "0 => <cero>" +
@@ -88,22 +88,82 @@ public class Cantador extends CantadorSpec {
             "2000000 => <millones[{1} millones_{0}]>"
         }};
 
+    private static final String[][] ordinalesFemeninos = {{"es_BO",
+            "1 => <primera>" +
+            "2 => <segunda>" +
+            "3 => <tercera>" +
+            "4 => <cuarta>" +
+            "5 => <quinta>" +
+            "6 => <sexta>" +
+            "7 => <séptima>" +
+            "8 => <octava>" +
+            "9 => <novena>" +
+            "10 => <décima>" +
+            "11 => <undécima>" +
+            "12 => <duodécima>" +
+            "13 => <decimotercera>" +
+            "14 => <decimocuarta>" +
+            "15 => <decimoquinta>" +
+            "16 => <decimosexta>" +
+            "17 => <decimoséptima>" +
+            "18 => <decimoctava>" +
+            "19 => <decimonovena>" +
+            "20 => <vigésimo[vigesimo{0}]>" +
+            "30 => <trigésimo[trigesimo{0}]>" +
+            "40 => <cuadragésimo[cuadragesimo{0}]>" +
+            "50 => <quincuagésimo[quincuagesimo{0}]>" +
+            "60 => <sexagésimo[sexagesimo{0}]>" +
+            "70 => <septuagésimo[septuagesimo{0}]>" +
+            "80 => <octogésimo[octogesimo{0}]>" +
+            "90 => <nonagésimo[nonagesimo{0}]>"
+        /*
+         * TODO: 100 => centesimo, 101 => centesimoprimera, 111 => centesimo undecima ??
+         */
+        }};
     /*
-         * TODO: http://en.wikipedia.org/wiki/Names_of_numbers_in_English
+     * TODO: http://en.wikipedia.org/wiki/Names_of_numbers_in_English
      */
     /*
      * TODO: in spanish: female form (doscientas instead of doscientos)
      * TODO: spanish: ordinal (male/female/"apocopado")
      */
+
     public static Cantador newCardinalInstance() {
         return newCardinalInstance(Locale.getDefault());
     }
 
+    /**
+     * @since 0.003
+     * @return Female ordinal form in default locale.
+     */
+    public static Cantador newFemaleOrdinalInstance() {
+        return newCardinalInstance(Locale.getDefault());
+    }
+
     public static Cantador newCardinalInstance(Locale locale) {
-        for (int i = 0; i < cardinals.length; i++) {
-            if (cardinals[i][0].equals(locale.toString())) {
+        return fromVariables(cardinals, locale);
+    }
+
+    /**
+     * Female ordinal form.
+     * @since 0.003
+     * @param locale currently only "ES_BO".
+     * @return a new instance for the bolivian spanish ordinal female.
+     * <p>Examples: </p>
+     * <ul>
+     *   <li>1 = primera.</li>
+     *   <li>11 = und&eacute;cima.</li>
+     * </ul>
+     */
+    public static Cantador newFemaleOrdinalInstance(Locale locale) {
+        return fromVariables(ordinalesFemeninos, locale);
+    }
+
+    private static final Cantador fromVariables(String[][] var, Locale locale) {
+        for (int i = 0; i < var.length; i++) {
+            if (var[i][0].equals(locale.toString())) {
                 try {
-                    return newInstance(cardinals[i][1]);
+                    return newInstance(var[i][1]);
                 } catch (ParseException ex) {
                     throw new UnsupportedOperationException("Locale " + locale.toString() + " bugged.", ex);
                 }
